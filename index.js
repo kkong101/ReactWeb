00930353c2002b942c94ee7cbe75bf7bc5c81cdc
kkong101 <aflhzv@gmail.com>
2021-05-06 00:35:41 +0900

    Setup Server

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
+const {User} = require('./models/User')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 mongoose.connect('mongodb+srv://test:test@cluster.rpjbh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => {
-    console.log('MongoDB Connected')
 })
-.catch(err => console.log(err))
+    .then(() => {
+        console.log('MongoDB Successfully Connected')
+    })
+    .catch(err => console.log(err))
 
 
-
-app.get('/', (req, res) => res.send('Hello'))
-
-app.listen(port, () => console.log(`example app listening on port ${port}!`))
+app.post('/register', (req,res) => {
+    const user = new User(req.body)
+    user.save((err,userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
