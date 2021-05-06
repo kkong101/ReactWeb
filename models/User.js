7e2b084c4c8e396602a0e7689d86b611dc94c006
kkong101 <aflhzv@gmail.com>
2021-05-06 22:16:44 +0900

    Token for MongoDB

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +35,58 @@ const userSchema = mongoose.Schema({
     }
 })
 
+
+userSchema.pre('save', (next) => {
+    var user = this;
+
+    if(user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if(err) return next(err);
+            
+            bcrypt.hash(user.password, salt, (err,hash) => {
+                if(err) return next(err)
+                user.password = hash;
+                next();
+            });
+        })
+    } else {
+        next();
+    }
+
+
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, userInfo) => {
+        if(!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "Don't have user information"
+            })
+        }
+
+        userInfo.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({ loginSuccess: false, message: "Wrong password"});
+            user.generateToken((err, user) => {
+                
+            })
+        })
+
+
+    })
+})
+
+
+userSchema.methods.comparePassword = (plainPassword, cb) => {
+    bcrypt.compare(plainPassword, this.password, (err,isMatch)=> {
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = (cb) => {
+
+}
 
 const User = mongoose.model('User', userSchema)
 
