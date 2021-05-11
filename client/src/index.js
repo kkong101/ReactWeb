5601e42cf0d5fad3adc6dcaff0aa9613f45e43c0
kkong101 <aflhzv@gmail.com>
2021-05-11 23:42:08 +0900

    Antd, Concurrently, Redux Setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux'
+import 'antd/dist/antd.css';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk'
+
+const createProxyMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider
+    store={}
+  >
+    <App/>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
