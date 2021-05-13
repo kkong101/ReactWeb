a7a4eba5a068c31b7d254c997dc5d94e2c443d11
kkong101 <aflhzv@gmail.com>
2021-05-13 17:43:58 +0900

    Logout 구현 완료

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import axios from 'axios'
 import {useEffect} from 'react'
+import { withRouter } from 'react-router-dom';
 
 
-export const LandingPage = () => {
+export const LandingPage = (props) => {
 
     useEffect(() => {
         return () => {
@@ -12,14 +13,30 @@ export const LandingPage = () => {
         }
     }, [])  
 
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if(response.data.success) {
+                    alert('로그아웃 성공');
+                    props.history.push('/login');
+                } else {
+                    alert('로그아웃 실패');
+                }
+            });
+    }
+
     return (
         <div style={{ 
             display: 'flex', justifyContent: 'center', alignItems: 'center',
              width: '100%', height: '100vh'
         }}>
             <h2>시작페이지</h2>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
 
-export default LandingPage
+
+export default withRouter(LandingPage)
